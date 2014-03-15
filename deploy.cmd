cd2c6232eec526e71c4491d4176a1c3024372af4
Kaveh Shahabi <kshahabi@usc.edu>
2014-03-14 22:48:09 -0700

    small fix on deploy script

--- deploy.cmd
+++ deploy.cmd
@@ -4,7 +4,7 @@
 set OLDDIR=%CD%
 SET "dir="D:\Archive (no backup)\arccasperdeploy""
 SET url=git@github.com:kaveh096/ArcCASPER.git
-::SET dropbox
+SET msg=""
 
 :: clean enviroment
 IF NOT EXIST %dir% ( git clone %url% %dir% )
@@ -19,18 +19,30 @@ for /f %%i in ('git rev-parse --short HEAD') do set hash=%%i
 
 :: check if there is new commit to be built
 for /f %%i in ('d:\cygwin64\bin\bash.exe -c "/home/kshahabi/dropbox_uploader.sh list | grep %hash% | wc -l"') do set iscommit=%%i
-if %iscommit%==1 ( exit 0 )
+if %iscommit%==1 (
+  SET "msg=No new code is commited to dev branch"
+  goto :exit
+)
 
 
 :: build clean
 set NOREG=1
-msbuild.exe /target:rebuild /p:Configuration=Release /p:Platform=Win32 USC.GISResearchLab.ArcCASPER.sln
-msbuild.exe /target:rebuild /p:Configuration=Release /p:Platform=x64   USC.GISResearchLab.ArcCASPER.sln
+del /Q EvcSolver32.*
+del /Q EvcSolver64.*
+::msbuild.exe /target:rebuild /p:Configuration=Release /p:Platform=Win32 USC.GISResearchLab.ArcCASPER.sln
+::msbuild.exe /target:rebuild /p:Configuration=Release /p:Platform=x64   USC.GISResearchLab.ArcCASPER.sln
 
 :: post to dropbox
 cd Package
 "c:\Program Files\7-Zip\7z.exe" a -bd ArcCASPER-nigthly-%hash%.zip EvcSolver32.dll EvcSolver64.dll glut32.dll glut64.dll Manual.pdf readme.txt  install.cmd uninstall.cmd
-d:\cygwin64\bin\bash.exe -c "/home/kshahabi/dropbox_uploader.sh upload \"$(cygpath -u \"%CD%\ArcCASPER-nigthly-%hash%.zip\")\" ArcCASPER-nigthly-%hash%.zip"
+if %ERRORLEVEL==0 (
+  d:\cygwin64\bin\bash.exe -c "/home/kshahabi/dropbox_uploader.sh upload \"$(cygpath -u \"%CD%\ArcCASPER-nigthly-%hash%.zip\")\" ArcCASPER-nigthly-%hash%.zip"
+) else (
+  SET "msg=Some of the files are not present"
+  goto :exit
+)
 
-:: restore current directory
+:: restore current directory and then exit
+:exit
+echo %msg%
 chdir /d %OLDDIR%
