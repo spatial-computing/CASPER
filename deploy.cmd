3e74f446f40e3f45fc95c6c80c46827a0ad48eff
Kaveh Shahabi <kshahabi@usc.edu>
2014-03-14 20:23:50 -0700

    deploy script v1

--- deploy.cmd
+++ deploy.cmd
@@ -14,9 +14,22 @@ cd %dir%
 git checkout dev
 git pull
 
+:: get hash of dev head
+for /f %%i in ('git rev-parse --short HEAD') do set hash=%%i
+
+:: check if there is new commit to be built
+for /f %%i in ('d:\cygwin64\bin\bash.exe -c "/home/kshahabi/dropbox_uploader.sh list | grep %hash% | wc -l"') do set iscommit=%%i
+if %iscommit%==1 ( exit 0 )
+
+
 :: build clean
-msbuild.exe /target:rebuild /p:Configuration=Debug /p:Platform=Win32 USC.GISResearchLab.ArcCASPER.sln
-msbuild.exe /target:rebuild /p:Configuration=Debug /p:Platform=x64" USC.GISResearchLab.ArcCASPER.sln
+msbuild.exe /target:rebuild /p:Configuration=Release /p:Platform=Win32 USC.GISResearchLab.ArcCASPER.sln
+msbuild.exe /target:rebuild /p:Configuration=Release /p:Platform=x64   USC.GISResearchLab.ArcCASPER.sln
+
+:: post to dropbox
+cd Package
+"c:\Program Files\7-Zip\7z.exe" a -bd ArcCASPER-nightly-%hash%.zip EvcSolver32.dll EvcSolver64.dll glut32.dll glut64.dll Manual.pdf readme.txt  install.cmd uninstall.cmd
+d:\cygwin64\bin\bash.exe -c "/home/kshahabi/dropbox_uploader.sh upload \"$(cygpath -u \"%CD%\ArcCASPER-nigthly-%hash%.zip\")\" ArcCASPER-nigthly-%hash%.zip"
 
 :: restore current directory
 chdir /d %OLDDIR%
