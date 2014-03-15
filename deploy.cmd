51d97b74f2f660355037597240dc21ce7c56391f
Kaveh Shahabi <kshahabi@usc.edu>
2014-03-15 08:51:26 -0700

    deploy script reads package list from file

--- deploy.cmd
+++ deploy.cmd
@@ -16,11 +16,48 @@ git pull
 
 :: get hash of dev head
 for /f %%i in ('git rev-parse --short HEAD') do set hash=%%i
+set out=ArcCASPER-nigthly-%hash%.zip
 
 :: check if there is new commit to be built
 for /f %%i in ('d:\cygwin64\bin\bash.exe -c "/home/kshahabi/dropbox_uploader.sh list | grep %hash% | wc -l"') do set iscommit=%%i
 if %iscommit%==1 (
-  SET "msg=No new code is commited to dev branch"
+  echo No new code is commited to dev branch
+  goto :stable
+)
+
+:: build clean
+set NOREG=1
+del /Q Package\EvcSolver32.*
+del /Q Package\EvcSolver64.*
+msbuild.exe /target:build /p:Configuration=Release /p:Platform=Win32 USC.GISResearchLab.ArcCASPER.sln
+msbuild.exe /target:build /p:Configuration=Release /p:Platform=x64   USC.GISResearchLab.ArcCASPER.sln
+
+:: post to dropbox
+cd Package
+"c:\Program Files\7-Zip\7z.exe" a %out%  -i@file.list
+if %errorlevel%==0 (
+  echo Uploading to dropbox for public access
+  d:\cygwin64\bin\bash.exe -c '/home/kshahabi/dropbox_uploader.sh upload `cygpath -u "%CD%\%out%"` %out%'
+) else (
+  echo Some of the files are not present
+)
+
+:stable
+:: checkout master for stable release
+
+:: update the dev branch
+cd %dir%
+git checkout master
+git pull
+
+:: get hash of dev head
+for /f %%i in ('git rev-parse --short HEAD') do set hash=%%i
+set out=ArcCASPER-stable-%hash%.zip
+
+:: check if there is new commit to be built
+for /f %%i in ('d:\cygwin64\bin\bash.exe -c "/home/kshahabi/dropbox_uploader.sh list | grep %hash% | wc -l"') do set iscommit=%%i
+if %iscommit%==1 (
+  set "msg=No new code is commited to master branch"
   goto :exit
 )
 
@@ -33,13 +70,12 @@ msbuild.exe /target:build /p:Configuration=Release /p:Platform=x64   USC.GISRese
 
 :: post to dropbox
 cd Package
-"c:\Program Files\7-Zip\7z.exe" a ArcCASPER-nigthly-%hash%.zip EvcSolver32.dll EvcSolver64.dll glut32.dll glut64.dll Manual.pdf readme.txt install.cmd uninstall.cmd
-if exist ArcCASPER-nigthly-%hash%.zip (
+"c:\Program Files\7-Zip\7z.exe" a %out%  -i@file.list
+if %errorlevel%==0 (
   echo Uploading to dropbox for public access
-  d:\cygwin64\bin\bash.exe -c '/home/kshahabi/dropbox_uploader.sh upload `cygpath -u "%CD%\ArcCASPER-nigthly-%hash%.zip"` ArcCASPER-nigthly-%hash%.zip'
+  d:\cygwin64\bin\bash.exe -c '/home/kshahabi/dropbox_uploader.sh upload `cygpath -u "%CD%\%out%"` %out%'
 ) else (
-  SET "msg=Some of the files are not present"
-  goto :exit
+  echo Some of the files are not present
 )
 
 :: restore current directory and then exit
