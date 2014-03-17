1d76e34dce3ee8ebb956b9929c560e4793a88e7e
Kaveh Shahabi <kshahabi@usc.edu>
2014-03-17 15:21:05 -0700

    deploy script updated to have revision number

--- deploy.cmd
+++ deploy.cmd
@@ -23,13 +23,13 @@ git checkout %1
 git pull
 
 :: get hash of head
-for /f %%i in ('git describe') do set hash=%%i
-set out=ArcCASPER-%2%-%rev%.zip
+for /f %%i in ('git describe') do set revi=%%i
+set "out=ArcCASPER-%revi%-%2%.zip"
 
 :: TODO create changle.log from 'git shortlog --no-merges %1 --not v10.1'
 
 :: check if there is new commit to be built
-for /f %%i in ('d:\cygwin64\bin\bash.exe -c "/home/kshahabi/dropbox_uploader.sh list | grep %rev% | wc -l"') do set iscommit=%%i
+for /f %%i in ('d:\cygwin64\bin\bash.exe -c "/home/kshahabi/dropbox_uploader.sh list | grep %revi% | wc -l"') do set iscommit=%%i
 if %iscommit%==1 (
   echo No new code is commited to %1% branch
   goto :eof
@@ -39,11 +39,11 @@ if %iscommit%==1 (
 set NOREG=1
 del /Q Package\EvcSolver32.*
 del /Q Package\EvcSolver64.*
-msbuild.exe /m /target:build /p:Configuration=Release /p:Platform=Win32 USC.GISResearchLab.ArcCASPER.sln
-msbuild.exe /m /target:build /p:Configuration=Release /p:Platform=x64   USC.GISResearchLab.ArcCASPER.sln
+::msbuild.exe /m /target:rebuild /p:Configuration=Release /p:Platform=Win32 USC.GISResearchLab.ArcCASPER.sln
+::msbuild.exe /m /target:rebuild /p:Configuration=Release /p:Platform=x64   USC.GISResearchLab.ArcCASPER.sln
 
 :: Generate readme file
-"c:\Program Files (x86)\GnuWin32\bin\sed.exe" -e "s/REV/%rev%/g" README.md > Package\README.md
+"c:\Program Files (x86)\GnuWin32\bin\sed.exe" -e "s/REV/%revi%/g" README.md > Package\README.md
 
 :: post to dropbox
 cd Package
@@ -60,4 +60,3 @@ goto :eof
 :: restore current directory and then exit
 :exit
 chdir /d %OLDDIR%
-exit 0
