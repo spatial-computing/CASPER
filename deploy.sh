8275dfcbaae55aafd7110955fc0b5fa7abfa2d55
Kaveh Shahabi <kshahabi@usc.edu>
2014-07-15 15:37:30 -0700

    switching to bash auto-deploy script

--- deploy.sh
+++ deploy.sh
@@ -21,15 +21,15 @@ do
   cd $tempdir
 
   # update / clone ther repo and switch to the right branch
-  if [ -d $repo_$branch ]; then
+  if [ -d "$repo-$branch" ]; then
     echo "repo already cloned. going to pull."
   else
     echo "repo will be cloned."
-    git clone git@github.com:kaveh096/$repo.git $repo_$branch
+    git clone git@github.com:kaveh096/$repo.git "$repo-$branch"
   fi
 
   # makeing sure repo is up-to-date
-  cd $repo_$branch
+  cd "$repo-$branch"
   git checkout $branch
   git pull
 
@@ -44,24 +44,28 @@ do
     set out=$repo-$revi-nightly.zip
   fi
 
-  if [ `$curdir/dropbox_uploader.sh list | grep %out% | wc -l` -eq 1 ]; then
+  if [ `$curdir/dropbox_uploader.sh list | grep $out | wc -l` -eq 1 ]; then
     echo "There are no new changes to be built"
   else
     echo "The $branch branch has new commits"
 
     # cleanup then rebuild
-    rm Package/EvcSolver32.* Package/EvcSolver64.* Package/README.md
-    "/cygdrive/c/Program Files (x86)/MSBuild/12.0/Bin/MSBuild.exe" /m /target:build /p:Configuration=Release /p:Platform=Win32 /p:NOREG=1 ArcCASPER.sln
-    "/cygdrive/c/Program Files (x86)/MSBuild/12.0/Bin/MSBuild.exe" /m /target:build /p:Configuration=Release /p:Platform=x64 /p:NOREG=1 ArcCASPER.sln
+    rm -f Package/EvcSolver32.* Package/EvcSolver64.* Package/README.md
+    "/cygdrive/c/Program Files (x86)/MSBuild/12.0/Bin/MSBuild.exe" /m /target:rebuild /p:Configuration=Release /p:Platform=Win32 /p:NOREG=1 ArcCASPER.sln
+    "/cygdrive/c/Program Files (x86)/MSBuild/12.0/Bin/MSBuild.exe" /m /target:rebuild /p:Configuration=Release /p:Platform=x64 /p:NOREG=1 ArcCASPER.sln
 
     # Generate readme file
-    sed -e "s/REV/%revi%/g" README.md > Package/README.md
+    sed -e "s/REV/$revi/g" README.md > Package/README.md
     
     # upload
     echo "Build is complete and ready for zipping and uploading"
     cd Package
-    "/cygdrive/c/Program Files/7-Zip/7z.exe" a %out% -i@file.list
-    $curdir/dropbox_uploader.sh upload %out%
+    "/cygdrive/c/Program Files/7-Zip/7z.exe" a $out -i@file.list
+    if [ $? -eq 0 ]; then
+      $curdir/dropbox_uploader.sh upload $out $out
+    else
+      echo "Some files are missing so we do not upload the zip file"
+    fi
   fi
 done
 cd $curdir
